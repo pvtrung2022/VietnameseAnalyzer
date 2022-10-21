a8342409cb6390351045b17720f4655ca1219a7f
Trung Van Pham <trung.vanpham@gmail.com>
2022-10-21 10:02:05 +0700

    Cập nhật dự án

--- VietnameseAnalyzer/out/production/VietnameseAnalyzer/src/vietnameseanalyzer/Test.java
+++ VietnameseAnalyzer/out/production/VietnameseAnalyzer/src/vietnameseanalyzer/Test.java
@@ -6,10 +6,47 @@ public class Test
 {
     public static void main(String[] argv)
     {
+        System.out.println("bắt đầu việc tải các thông tin cần thiết");
         VietnameseAnalyzer.initialize();
-        System.out.println("finished initialization");
-        var sentence="tôi yêu cô ấy rất nhiều";
-        var parser=VietnameseAnalyzer.decideParser(sentence);
-        System.out.println(parser);
+        System.out.println("hoàn tất việc tải các thông tin cần thiết");
+
+        // đây là trường hợp nhập câu trực tiếp
+        {
+            var sentence = "tôi yêu cô ấy rất nhiều";
+            var parser = VietnameseAnalyzer.decideParser(sentence);
+            System.out.println("in kết quả cho trường hợp nhập câu trực tiếp");
+            System.out.println(parser);
+        }
+
+        // đây là trường hợp nhập câu theo mảng
+        {
+            var sentence = new String[][]{
+                    {"tôi", "P"},
+                    {"yêu", "V"},
+                    {"cô", "N"},
+                    {"ấy", "P"},
+                    {"rất", "R"},
+                    {"nhiều", "A"}
+            };
+            var parser = VietnameseAnalyzer.decideParser(sentence);
+            System.out.println("in kết quả cho trường hợp nhập câu theo mảng");
+            System.out.println(parser);
+        }
+
+        // kiểm tra một từ có trong dữ liệu huấn luyện hay không
+        {
+            var word = "chăm chỉ";
+            var check = VietnameseAnalyzer.WordAppearances.containsKey(word);
+            if (check)
+            {
+                System.out.println("từ '" + word + "'" + " này có trong dữ liệu huấn luyện");
+                System.out.println("các kiểu ngữ pháp của từ này là: " +
+                        VietnameseAnalyzer.WordAppearances.get(word).keySet()
+                );
+                System.out.println("số lần xuất hiện của từ này trong dữ liệu huấn luyện: " +
+                        VietnameseAnalyzer.WordAppearances.get(word)
+                );
+            } else System.out.println("từ này không có trong dữ liệu huấn luyện");
+        }
     }
 }
